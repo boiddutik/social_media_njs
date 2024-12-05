6121629cf26d4a5f4e2e2e5178bcb9bef878cf37
Nuaiman Ashiq <ashiqnuaiman@gmail.com>
2024-12-05 19:56:19 +0600

    commit

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -3,17 +3,11 @@ import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 
-// Derive the current directory path using import.meta.url
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-// Define the root directory where public/temp will be created
-const rootDir = path.resolve(__dirname, "../../"); // Resolves to the project root folder
-
-// Define the upload directory relative to the root folder
+const rootDir = path.resolve(__dirname, "../../");
 const uploadDir = path.join(rootDir, "public", "temp");
 
-// Ensure the directory exists, if not, create it
 try {
     fs.mkdirSync(uploadDir, { recursive: true });
     console.log(`Directory ${uploadDir} created or already exists.`);
@@ -21,22 +15,18 @@ try {
     console.error("Error creating directory:", error);
 }
 
-// Set up multer storage configuration
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        // Specify the upload directory for the files
         cb(null, uploadDir);
     },
     filename: (req, file, cb) => {
-        // Generate a unique filename based on timestamp
         cb(null, `${Date.now()}-${file.originalname}`);
     },
 });
 
-// Set up multer upload configuration
 export const upload = multer({
     storage,
-    limits: { fileSize: 500 * 1024 * 1024 }, // Limit file size to 500MB
+    limits: { fileSize: 500 * 1024 * 1024 },
 }).fields([
     { name: "avatar", maxCount: 1 },
     { name: "cover", maxCount: 1 },
