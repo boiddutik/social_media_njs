853a2a4a951885ab8c2ed231c1dedaddb7239c65
Nuaiman Ashiq <ashiqnuaiman@gmail.com>
2024-12-07 04:21:12 +0600

    commit

--- src/app.js
+++ src/app.js
@@ -12,11 +12,21 @@ app.use(cookieParser())
 // --
 import userRouter from "./routes/user.routes.js"
 import profileRouter from "./routes/profile.routes.js"
+import postRouter from "./routes/post.routes.js"
+import gymRouter from "./routes/gym.routes.js"
+import conversationRouter from "./routes/conversation.routes.js"
+import commentRouter from "./routes/comment.routes.js"
+import chatRouter from "./routes/chat.routes.js"
 // --
 app.use("/api/v1/ping", (_, res) => {
     return res.send("Backend is Running")
 })
 app.use("/api/v1/user", userRouter)
 app.use("/api/v1/profile", profileRouter)
+app.use("/api/v1/post", postRouter)
+app.use("/api/v1/gym", gymRouter)
+app.use("/api/v1/conversation", conversationRouter)
+app.use("/api/v1/comment", commentRouter)
+app.use("/api/v1/chat", chatRouter)
 
 export { app };
